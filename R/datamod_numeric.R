6f1fce0acd3ed46ad67733b94b1d26aa7971e766
Tom Elliott <tomelliottnz@gmail.com>
2017-10-02 14:14:26 +1300

    Version bump 1.4-1; CRAN checks

--- R/datamod_numeric.R
+++ R/datamod_numeric.R
@@ -7,6 +7,8 @@
 ##' @return a dataframe with the new variable, along with the code used to generate it
 ##' @author Tom Elliott
 ##'
+##' @import tibble
+##' @import magrittr
 ##' @export
 numToCat <- function(.dataset, var, name = sprintf("%s.cat", var)) {
     exp <- as.formula(sprintf(
@@ -17,6 +19,16 @@ numToCat <- function(.dataset, var, name = sprintf("%s.cat", var)) {
 }
 
 
+##' Transform a numeric variable
+##'
+##' @title Transform Numeric Variable
+##' @param .dataset the dataset to use
+##' @param var character, name of the numeric variable
+##' @param fn character, name of the function to call
+##' @param name name of the created variable (defaults to {original}.{fn})
+##' @return a dataframe with the new variable(, along with the code used to generate it)
+##'
+##' @author Tom Elliott
 ##' @export
 transform <- function(.dataset, var, fn, name = sprintf("%s.%s", fn, var)) {
     x <- switch(fn,
@@ -28,6 +40,6 @@ transform <- function(.dataset, var, fn, name = sprintf("%s.%s", fn, var)) {
     data[[name]] <- round(x, 3)
     if (w < ncol(.dataset))
         data <- cbind(data, .dataset[, -(1:w)])
-                      
+
     data
 }

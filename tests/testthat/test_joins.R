0a80299c531bf226d6e8aff35ba1081f1e1957d5
yiwen16 <yhe443@aucklanduni.ac.nz>
2019-02-18 22:41:52 +1300

    getting rid of attributes for testing

--- tests/testthat/test_joins.R
+++ tests/testthat/test_joins.R
@@ -1,14 +1,18 @@
 context("Test data joins")
 
-stripcode <- function(x) {
-  attributes(x)$code <- NULL
+stripattr <- function(x, attrs = c('code', 'join_cols')) {
+  for (attr in attrs) attributes(x)[[attr]] <- NULL
   x
 }
 
-d1 <- read_csv('Join.csv')
-d3 <- read_csv('Join2.csv')
-d2 <- d3 %>% select(x1, x3)
+data = readr::read_csv("~/iNZight/scripts/parisjoin.csv") #original
+data2 = readr::read_csv("~/iNZight/scripts/parisjoin2.csv") #has the same column "Instagram photo" with completely different values
+data3 = readr::read_csv("~/iNZight/scripts/parisjoin3.csv") #contains a column named "sdasdsds" which has the same value as "Instagram user"
+data4 = readr::read_csv("~/iNZight/scripts/parisjoin4.csv") #contains the same column "Instagram photo" with one value that is the same to the original one
 
 test_that("Auto detection works", {
-  expect_equal(stripcode(joindata(d1, d2)), inner_join(d1, d2))
+  expect_equal(stripattr(joindata(d1, d2)), inner_join(d1, d2))
 })
+
+# 
+

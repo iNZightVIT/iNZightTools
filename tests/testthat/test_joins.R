e00e3493dd3e4fb02aa381a446f39403236cef72
Tom Elliott <tomelliottnz@gmail.com>
2019-03-18 11:43:53 +1300

    Fix up tests

--- tests/testthat/test_joins.R
+++ tests/testthat/test_joins.R
@@ -5,16 +5,19 @@ stripattr <- function(x, attrs = c('code', 'join_cols')) {
   x
 }
 
-d1 = readr::read_csv("~/iNZight/scripts/join.csv")
-d2 = readr::read_csv("~/iNZight/scripts/join2.csv") 
-d3 = readr::read_csv("~/iNZight/scripts/join3.csv") 
+d1 <- readr::read_csv("join.csv")
+d2 <- readr::read_csv("join2.csv") 
+d3 <- readr::read_csv("join3.csv") 
 
 test_that("Auto detection works", {
   expect_equal(stripattr(joindata(d1, d2, "", "", "inner_join", "x", "y")), inner_join(d1, d2))
 })
 
 test_that("Inner join works", {
-  expect_equal(stripattr(joindata(d1, d3, list("x1", "x2"), list("x1", "x6"), "inner_join", "x", "y")), inner_join(d1, d3, by = c("x1" = "x1", "x2" = "x6")))
+  expect_equal(
+      stripattr(joindata(d1, d3, list("x1", "x2"), list("x1", "x6"), "inner_join", "x", "y")), 
+      inner_join(d1, d3, by = c("x1" = "x1", "x2" = "x6"))
+  )
   expect_equal(stripattr(joindata(d1, d2, "x1", "x1", "inner_join", "x", "y")), inner_join(d1, d2, by = c("x1" = "x1")))
 })
 

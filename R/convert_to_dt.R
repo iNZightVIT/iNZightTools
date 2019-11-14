90725598f5ace1f761e7e672b43d9725479b5fa6
Tom Elliott <tomelliottnz@gmail.com>
2019-11-14 20:01:03 +1300

    Reformat/clean more files/functions

--- R/convert_to_dt.R
+++ R/convert_to_dt.R
@@ -1,5 +1,3 @@
-# Convert a variable to dates and times
-
 #' Convert to datetime
 #'
 #' @param .data dataframe
@@ -10,58 +8,64 @@
 #' @return dataframe with datatime column
 #' @export
 #' @author Yiwen He
-
-# Something to think about
-#     converted <- tryCatch(
-#       lubridate::parse_date_time(varx, convert),
-#       warning = function(w) if (w$message != "All formats failed to parse. No formats found.") warning(w$message) else return(NA)
-
 convert_to_datetime <- function(.data, factorname, convname, newname) {
-  
-  mc <- match.call()
-  dataname <- mc$.data
-  
-  ## Subsetting dataset to get varx
-  Fname = paste0(".DATA$'", factorname, "'", collapse = ", ")
-  Fname = paste0("paste(", Fname,  ")")
-  
-  ## Working out the convert format
-  order_split = strsplit(convname, " ")
-  convert.string = ""
-  for (i in order_split) {
-    convert.string = paste(convert.string, "%", substring(i, 1, 1), sep = "", collapse = "")
-  }
-  
-  ## Actual function
-  if (convert.string == "%U%t%(%f%1") {
-    Fname = paste0("as.numeric(", Fname, ")")
-    exp <- ~.DATA %>% 
-      tibble::add_column(.NAME = as.POSIXct(.VARX, origin = "1970-01-01"), .after = ".AFTER")
-  } else {
-    exp <- ~.DATA %>% 
-      tibble::add_column(.NAME = lubridate::parse_date_time(.VARX, convert), .after = ".AFTER")
-  }
 
-  ## Replacing variables
-  exp <- replaceVars(exp, 
-                    .NAME = newname,
-                    .VARX = Fname,
-                    .DATA = dataname,
-                    .VARNAME = factorname,
-                    .AFTER = factorname[length(factorname)]
-                    )
-  
-  tryCatch(
-    interpolate(exp, convert = convert.string),
-    warning = function(w) {
-      if (w$message == "All formats failed to parse. No formats found." | substr(w$message,1,10) == "PCRE patte"){
-        warning("Failed to parse")
-        return(.data)
-      } else {
-        warning(w$message)
-      }
+    mc <- match.call()
+    dataname <- mc$.data
+
+    ## Subsetting dataset to get varx
+    fact_name <- paste0(".DATA$'", factorname, "'", collapse = ", ")
+    fact_name <- paste0("paste(", fact_name,  ")")
+
+    ## Working out the convert format
+    order_split <- strsplit(convname, " ")
+    convert_string <- ""
+    for (i in order_split) {
+        convert_string <- paste(
+            convert_string,
+            "%",
+            substring(i, 1, 1),
+            sep = "",
+            collapse = ""
+        )
     }
-  )
-  
-}
 
+    ## Actual function
+    if (convert_string == "%U%t%(%f%1") {
+        fact_name <- paste0("as.numeric(", fact_name, ")")
+        exp <- ~.DATA %>%
+            tibble::add_column(
+                .NAME = as.POSIXct(.VARX, origin = "1970-01-01"),
+                .after = ".AFTER"
+            )
+    } else {
+        exp <- ~.DATA %>%
+            tibble::add_column(
+                .NAME = lubridate::parse_date_time(.VARX, convert),
+                .after = ".AFTER"
+            )
+    }
+
+    ## Replacing variables
+    exp <- replaceVars(exp,
+        .NAME = newname,
+        .VARX = Fname,
+        .DATA = dataname,
+        .VARNAME = factorname,
+        .AFTER = factorname[length(factorname)]
+    )
+
+    tryCatch(
+        interpolate(exp, convert = convert_string),
+        warning = function(w) {
+            if (w$message == "All formats failed to parse. No formats found." |
+                substr(w$message, 1, 10) == "PCRE patte") {
+                warning("Failed to parse")
+                return(.data)
+            } else {
+                warning(w$message)
+            }
+        }
+    )
+
+}

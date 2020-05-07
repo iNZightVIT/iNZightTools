4a4cc00ae30d71ed16a5de63f5ca67125582937f
Tom Elliott <tomelliottnz@gmail.com>
2020-05-08 10:57:09 +1200

    Use forcats::fct_cross instead for combineCatVars

--- tests/testthat/test_catvars.R
+++ tests/testthat/test_catvars.R
@@ -6,3 +6,20 @@ test_that("Combine categorical variables returns a factor", {
     cas2 <- combineCatVars(cas, c("travel", "gender"))
     expect_is(cas2$travel.gender, "factor")
 })
+
+test_that("NAs in either variable return an NA", {
+    cas2 <- combineCatVars(cas, c("gender", "cellsource"))
+    expect_equal(
+        sum(is.na(cas2$gender.cellsource)),
+        sum(is.na(cas2$gender) | is.na(cas2$cellsource))
+    )
+    cas2 <- combineCatVars(cas, c("getlunch", "cellsource"), keep_empty = TRUE)
+    expect_equal(
+        sum(is.na(cas2$getlunch.cellsource)),
+        sum(is.na(cas2$getlunch) | is.na(cas2$cellsource))
+    )
+    expect_equal(
+        table(cas2$getlunch.cellsource)[["school.other"]],
+        0
+    )
+})

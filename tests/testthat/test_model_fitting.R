0960141091c3bcdfa236e6a27cdb22dceabbd0e8
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2020-06-18 18:59:07 +1200

    Negative binomial tests

--- tests/testthat/test_model_fitting.R
+++ tests/testthat/test_model_fitting.R
@@ -17,3 +17,15 @@ test_that("Family and link arguments are included", {
         "glm(response ~ x1, data = d, family = binomial(link = \"probit\"))"
     )
 })
+
+test_that("Negative binomial regression works", {
+    expect_equal(
+        fitModel("response", "x1", "d", family = "negbin"),
+        "MASS::glm.nb(response ~ x1, data = d)"
+    )
+    
+    expect_equal(
+        fitModel("response", "x1", "d", family = "negbin", link = "sqrt"),
+        "MASS::glm.nb(response ~ x1, data = d, link = \"sqrt\")"
+    )
+})

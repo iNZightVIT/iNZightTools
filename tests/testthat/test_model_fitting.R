604c396a40f2e22a2d11c85e3da332e63b89aa8a
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2020-06-18 19:13:22 +1200

    A few more negbin tests

--- tests/testthat/test_model_fitting.R
+++ tests/testthat/test_model_fitting.R
@@ -19,13 +19,27 @@ test_that("Family and link arguments are included", {
 })
 
 test_that("Negative binomial regression works", {
+    ## Basic NegBin model
     expect_equal(
         fitModel("response", "x1", "d", family = "negbin"),
         "MASS::glm.nb(response ~ x1, data = d)"
     )
     
+    ## Link should be added if not log
     expect_equal(
         fitModel("response", "x1", "d", family = "negbin", link = "sqrt"),
         "MASS::glm.nb(response ~ x1, data = d, link = \"sqrt\")"
+    )
+    
+    ## xargs come through
+    expect_equal(
+        fitModel("response", "x1", "d", family = "negbin", model = FALSE),
+        "MASS::glm.nb(response ~ x1, data = d, model = FALSE)"
+    )
+    
+    ## Survey designs currently unsupported
+    expect_error(
+        fitModel("response", "x1", "d", family = "negbin", design = "survey"),
+        "not yet implemented for survey designs"
     )
 })

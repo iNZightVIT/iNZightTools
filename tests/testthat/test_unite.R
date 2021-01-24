44502975d9901ecb19526b002ee5c6ee4a858ca8
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-01-25 09:11:27 +1300

    Refactor code formulation for `unite()`; coerce result to factor

--- tests/testthat/test_unite.R
+++ tests/testthat/test_unite.R
@@ -1,10 +1,12 @@
 context("Unite columns")
 
 cas <- smart_read("cas500.csv")
+# cas <- smart_read("tests/testthat/cas500.csv")
 
 test_that("Either united column NAs remain as NAs", {
     cas2 <- unite(cas, "new", c("getlunch", "cellsource"), sep = "_")
-    # expect_is(cas2$new, "factor")
+    expect_is(cas2$new, "factor")
+    expect_equivalent(eval(parse(text = code(cas2))), cas2)
 })
 
 require(survey)
@@ -14,5 +16,6 @@ svy <- svydesign(~dnum+snum, weights = ~pw, fpc = ~fpc1+fpc2, data = apiclus2)
 test_that("Uniting variables works with survey object", {
     d <- unite(svy, "new", c("enroll", "awards"), sep = "&")
     expect_is(d, "survey.design2")
+    expect_is(d$variables$new, "factor")
     expect_equivalent(eval(parse(text = code(d))), d)
 })

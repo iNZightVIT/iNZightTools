a01e194fbf3b0ce88e81b15db0e990ce6214809a
Tom Elliott <tomelliottnz@gmail.com>
2021-01-26 12:54:56 +1300

    add tests for appendrows

---
+++ tests/testthat/test_appendrows.R
@@ -0,0 +1,17 @@
+context("Append rows")
+
+cas <- smart_read("cas500.csv")
+# cas <- smart_read("tests/testthat/cas500.csv")
+
+test_that("Rows can be appended", {
+    expect_silent(c2 <- appendrows(cas[1:400,], cas[-(1:400),]))
+    expect_equivalent(c2, cas)
+})
+
+require(survey)
+data(api)
+svy <- svydesign(~dnum+snum, weights = ~pw, fpc = ~fpc1+fpc2, data = apiclus2)
+
+test_that("Survey returns error", {
+    expect_error(appendrows(svy, cas), "Cannot append rows to surveys")
+})

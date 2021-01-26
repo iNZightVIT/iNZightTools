e9202214823bd611f7ec763bd7d2c1318a6e72a9
Tom Elliott <tomelliottnz@gmail.com>
2021-01-26 13:09:47 +1300

    add test for survey collapseLevels()

--- tests/testthat/test_collapseLevels.R
+++ tests/testthat/test_collapseLevels.R
@@ -9,7 +9,7 @@ test_that("Collapsing 'string' factors", {
         "data.frame"
     )
     expect_equal(
-        levels(cas1$travel.coll), 
+        levels(cas1$travel.coll),
         c("bike_bus", "motor", "other", "train", "walk")
     )
 })
@@ -25,4 +25,21 @@ test_that("Collapsing 'number' factors", {
         levels(cas2$year.cat.coll),
         c(4:10, "13_12_11")
     )
+})
+
+
+require(survey)
+data(api)
+svy <- svydesign(~dnum+snum, weights = ~pw, fpc = ~fpc1+fpc2, data = apiclus2)
+
+test_that("Survey collapsing works", {
+    expect_silent(
+        d <- collapseLevels(svy, "stype", c("E", "H"))
+    )
+    expect_is(d, "survey.design2")
+    expect_equal(
+        d$variables$stype.coll,
+        forcats::fct_collapse(d$variables$stype, E_H = c("E", "H"))
+    )
+    expect_equivalent(eval(parse(text = code(d))), d)
 })

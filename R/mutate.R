c0b1bbe99a071806559ae508335b7b5a9ab9a770
szmsu2011 <szmsu2011@hotmail.co.nz>
2023-01-06 22:19:58 +1300

    modify, create and delete variables with mutate function

--- R/mutate.R
+++ R/mutate.R
@@ -141,3 +141,152 @@ mutate_as_datetime <- function(data, vars, names = NULL, tz = "") {
     expr <- mutate_expr(expr, vars_expr, data)
     eval_code(expr)
 }
+
+
+#' Convert variables to dates
+#'
+#' @param data a dataframe with the variables to convert
+#' @param vars a character vector of column names to convert
+#' @param names a character vector of names for the created variables
+#' @return original dataframe containing new columns of the
+#'         converted variables with tidyverse code attached
+#' @rdname mutate_as_date
+#' @seealso \code{\link{code}}
+#' @export
+#' @author Stephen Su
+mutate_as_date <- function(data, vars, names = NULL) {
+    expr <- rlang::enexpr(data)
+    if (is.null(names)) {
+        names <- sprintf("%s.date", vars)
+    }
+    vars_expr <- purrr::map(vars, function(x) {
+        rlang::expr(lubridate::as_date(!!rlang::sym(x)))
+    }) |> rlang::set_names(names)
+    expr <- mutate_expr(expr, vars_expr, data)
+    eval_code(expr)
+}
+
+
+#' Create new variables
+#'
+#' Create new variables by using valid R expressions and returns
+#' the result along with tidyverse code used to generate it.
+#'
+#' @param data a dataframe to which to add new variables to
+#' @param vars a character of the new variable names
+#' @param vars_expr a character of valid R expressions which can
+#'        generate vectors of values
+#' @return original dataframe containing the new columns
+#'         created from \code{vars_expr}
+#' with tidyverse code attached
+#' @seealso \code{\link{code}}
+#' @examples
+#' created <- mutate_new(
+#'     data = iris,
+#'     vars = "Sepal.Length_less_Sepal.Width",
+#'     "Sepal.Length - Sepal.Width"
+#' )
+#' cat(code(created))
+#' head(created)
+#' @rdname mutate_new
+#' @author Stephen Su
+#' @export
+mutate_new <- function(data, vars = ".new_var", vars_expr = NULL) {
+    expr <- rlang::enexpr(data)
+    vars_expr <- purrr::map(vars_expr, rlang::parse_expr) |>
+        rlang::set_names(vars)
+    expr <- mutate_expr(expr, vars_expr, data)
+    eval_code(expr)
+}
+
+
+#' Delete variables from a dataset
+#'
+#' @title Delete variables
+#' @param data dataset
+#' @param vars variable names to delete
+#' @return dataset without chosen variables
+#' @author Stephen Su
+#' @rdname delete_vars
+#' @export
+delete_vars <- function(data, vars = NULL) {
+    expr <- rlang::enexpr(data)
+    vars_expr <- replicate(length(vars), NULL) |>
+        rlang::set_names(vars)
+    expr <- mutate_expr(expr, vars_expr, data)
+    eval_code(expr)
+}
+
+
+#' Transform data of numeric variables
+#'
+#' Transform the values of numeric variables by applying
+#' a mathematical function
+#' @param data a dataframe with the variables to transform
+#' @param vars  a character of the numeric variables in \code{data} to transform
+#' @param fn  the name (a string) of a valid R function
+#' @param names the names of the new variables
+#'
+#' @return the original dataframe containing the new columns of the transformed
+#'         variable with tidyverse code attached
+#' @rdname mutate_transform
+#' @seealso \code{\link{code}}
+#' @examples
+#' transformed <- mutate_transform(iris,
+#'     var = "Petal.Length",
+#'     fn = "log"
+#' )
+#' cat(code(transformed))
+#' head(transformed)
+#'
+#' @author Stephen Su
+#' @export
+mutate_transform <- function(data, vars, fn, names = NULL) {
+    expr <- rlang::enexpr(data)
+    if (is.null(names)) {
+        names <- sprintf("%s.%s", fn, vars)
+    }
+    vars_expr <- purrr::map(vars, function(x) {
+        rlang::expr((!!rlang::parse_expr(fn))(!!rlang::sym(x)))
+    }) |> rlang::set_names(names)
+    expr <- mutate_expr(expr, vars_expr, data)
+    eval_code(expr)
+}
+
+
+#' Rename column names
+#'
+#' Rename columns of a dataset with desired names
+#'
+#' @param data a dataframe with columns to rename
+#' @param from a character vector the variable names as-is
+#' @param to a character vector the variable names to-be
+#' @return original dataframe containing new columns of the renamed columns
+#'        with tidyverse code attached
+#' @rdname rename_vars
+#' @seealso \code{\link{code}}
+#' @examples
+#' renamed <- rename_vars(iris,
+#'     from = names(iris),
+#'     to = tolower(gsub("\\.", "_", names(iris)))
+#' )
+#' cat(code(renamed))
+#' head(renamed)
+#'
+#' @author Stephen Su
+#' @export
+rename_vars <- function(data, from, to) {
+    expr <- rlang::enexpr(data)
+    if (length(from) != length(to)) {
+        rlang::abort("`from` and `to` must have same length.")
+    }
+    vars_expr <- purrr::map(from, rlang::sym) |>
+        rlang::set_names(to)
+    if (is_survey(data)) {
+        expr <- coerce_tbl_svy(expr, data)
+        expr <- rlang::expr(!!expr %>% srvyr::rename(!!!vars_expr))
+    } else {
+        expr <- rlang::expr(!!expr %>% dplyr::rename(!!!vars_expr))
+    }
+    eval_code(expr)
+}

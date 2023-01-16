aa83299ba375953f85b9d9e8665ede357489df13
Tom Elliott <tomelliottnz@gmail.com>
2023-01-16 14:15:53 +1300

    use install_deps for all workflows

---
+++ .github/install_deps.R
@@ -0,0 +1,32 @@
+# R script
+github_deps <- c(
+    "tmelliott/surveyspec@develop"
+)
+
+if (.Platform$OS.type == "windows") {
+    install.packages("RCurl", type = "binary")
+}
+
+OS <- Sys.getenv("OS_TYPE")
+if (OS == "Windows" && !requireNamespace("RCurl", quietly = TRUE)) {
+    install.packages("RCurl", repos = "https://cran.r-project.org")
+}
+
+options(
+    repos = c(
+        RSPM = Sys.getenv("RSPM"),
+        CRAN = "https://cloud.r-project.org"
+    ),
+    install.packages.compile.from.source = "never"
+)
+
+remotes::install_github(github_deps,
+    INSTALL_opts = c("--no-multiarch")
+)
+remotes::install_deps(
+    dependencies = TRUE,
+    INSTALL_opts = c("--no-multiarch")
+)
+remotes::install_cran("rcmdcheck",
+    INSTALL_opts = c("--no-multiarch")
+)

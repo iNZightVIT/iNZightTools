520d07695f75f602f5f3c6ba789a62e3fb711e8b
szmsu2011 <szmsu2011@hotmail.co.nz>
2023-01-24 07:50:25 +1300

    more mutate-related functions

--- R/mutate.R
+++ R/mutate.R
@@ -354,3 +354,131 @@ standardize_vars <- function(data, vars, names = NULL) {
     expr <- mutate_expr_i(expr, vars_expr, data, .after = vars)
     eval_code(expr)
 }
+
+
+#' Rank the data of numeric variables
+#'
+#' Rank the values of numeric variables, for example, in descending order,
+#' and then returns the result along with tidyverse code used to generate it.
+#' For all the types of ranking see \code{\link[dplyr]{ranking}}.
+#'
+#' @param data a dataframe with the variables to rank
+#' @param vars  a character vector of numeric variables in \code{data}
+#'        to rank
+#' @param rank_type either \code{"min"}, \code{"dense"} or \code{"percent"},
+#'        for more details see \code{\link[dplyr]{ranking}}
+#'
+#' @return the original dataframe containing new columns with the ranks of the
+#'         variables in \code{vars} with tidyverse code attached
+#' @rdname rank_vars
+#' @seealso \code{\link{code}}
+#' @examples
+#' ranked <- rank_vars(iris, vars = c("Sepal.Length", "Petal.Length"))
+#' cat(code(ranked))
+#' head(ranked)
+#'
+#' @author Stephen Su
+#' @export
+rank_vars <- function(data, vars, rank_type = c("min", "dense", "percent")) {
+    rank_type <- rlang::arg_match(rank_type)
+    expr <- rlang::enexpr(data)
+    rank_fn <- sprintf("dplyr::%s_rank", rank_type)
+    vars_expr <- purrr::map(vars, function(x) {
+        rlang::expr((!!rlang::parse_expr(rank_fn))(!!rlang::sym(x)))
+    }) |> rlang::set_names(sprintf("%s.%s_rank", vars, rank_type))
+    expr <- mutate_expr_i(expr, vars_expr, data, .after = vars)
+    eval_code(expr)
+}
+
+
+#' Rename the levels of a categorical variable
+#'
+#' Rename the levels of a categorical variables, and returns the result
+#' along with tidyverse code used to generate it.
+#'
+#' @param data a dataframe with the column to be renamed
+#' @param var  a character of the categorical variable to rename
+#' @param tobe_asis  a named list of the old level names assigned
+#'        to the new level names
+#'        ie. list('new level names' = 'old level names')
+#' @param name a name for the new variable
+#'
+#' @return original dataframe containing a new column of the renamed categorical
+#'         variable with tidyverse code attached
+#' @rdname rename_levels
+#'
+#' @seealso \code{\link{code}}
+#' @examples
+#' renamed <- rename_levels(iris,
+#'     var = "Species",
+#'     tobe_asis = list(set = "setosa", ver = "versicolor")
+#' )
+#' cat(code(renamed))
+#' head(renamed)
+#'
+#' @author Stephen Su
+#' @export
+rename_levels <- function(data, var, tobe_asis, name = NULL) {
+    expr <- rlang::enexpr(data)
+    if (is.null(name)) {
+        name <- sprintf("%s.renamed", var)
+    }
+    vars_expr <- rlang::list2(!!name := rlang::expr(
+        forcats::fct_recode(!!rlang::sym(var), !!!tobe_asis)
+    ))
+    expr <- mutate_expr_i(expr, vars_expr, data, .after = var)
+    eval_code(expr)
+}
+
+
+#' Reorder the levels of a categorical variable
+#'
+#' Reorder the levels of a categorical variable either manually or automatically
+#'
+#' @param data a dataframe to reorder
+#' @param var a categorical variable to reorder
+#' @param new_levels a character vector of the new factor order;
+#'        overrides \code{auto} if not \code{NULL}
+#' @param auto only meaningful if \code{new_levels} is \code{NULL}: the method
+#'        to auto-reorder the levels, see \code{\link[forcats]{fct_inorder}}
+#' @param name name for the new variable
+#'
+#' @return original dataframe containing a new column of the reordered
+#'         categorical variable with tidyverse code attached
+#' @rdname reorder_levels
+#' @seealso \code{\link{code}}
+#' @examples
+#' reordered <- reorder_levels(iris,
+#'     var = "Species",
+#'     new_levels = c("versicolor", "virginica", "setosa")
+#' )
+#' cat(code(reordered))
+#' head(reordered)
+#'
+#' reordered <- reorder_levels(iris,
+#'     var = "Species",
+#'     auto = "freq"
+#' )
+#' cat(code(reordered))
+#' head(reordered)
+#'
+#' @author Stephen Su
+#' @export
+reorder_levels <- function(data, var, new_levels = NULL,
+                           auto = c("freq", "order", "seq"),
+                           name = NULL) {
+    expr <- rlang::enexpr(data)
+    new_lvls <- rlang::enexpr(new_levels)
+    if (is.null(new_levels)) {
+        auto <- sprintf("forcats::fct_in%s", rlang::arg_match(auto))
+        rhs_expr <- rlang::expr((!!rlang::parse_expr(auto))(!!rlang::sym(var)))
+    } else {
+        rhs_expr <- rlang::expr(factor(!!rlang::sym(var), !!new_lvls))
+    }
+    if (is.null(name)) {
+        name <- sprintf("%s.reord", var)
+    }
+    vars_expr <- rlang::list2(!!name := rhs_expr)
+    expr <- mutate_expr_i(expr, vars_expr, data, .after = var)
+    eval_code(expr)
+}

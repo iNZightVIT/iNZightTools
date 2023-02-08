6385382748047b4d09417cab743844189e92991a
szmsu2011 <szmsu2011@hotmail.co.nz>
2023-02-08 16:54:09 +1300

    allow multi-column separation

--- R/separate_var.R
+++ R/separate_var.R
@@ -2,32 +2,37 @@
 #'
 #' @param data dataset
 #' @param var name of variable to be separated
-#' @param by a string as delimiter between values (separate by delimiter) or an
-#'        integer as number of characters to split by (separate by position)
-#' @param left name for the separated left column (for \code{into = "cols"})
-#' @param right name for the separated right column (for \code{into = "cols"})
+#' @param by a string as delimiter between values (separate by delimiter) or
+#'        integer(s) as number of characters to split by (separate by position),
+#'        the length of \code{by} should be \code{1} unless \code{by} is integer
+#'        and \code{into = "cols"}; if \code{by} is a non-integer numeric vector
+#'        its values will be rounded down to the nearest integer
+#' @param names for \code{into = "cols"}, a character vector of output column
+#'        names; use \code{NA} if there are components that you don't want to
+#'        appear in the output; the number of non-\code{NA} elements determines
+#'        the number of new columns in the result
 #' @param into whether to split into new rows or columns
 #'
 #' @return Separated dataset
 #' @rdname separate_var
 #' @export
 #' @author Stephen Su
-separate_var <- function(data, var, by, left, right,
-                         into = c("cols", "rows")) {
+separate_var <- function(data, var, by, names, into = c("cols", "rows")) {
     expr <- rlang::enexpr(data)
     into <- ifelse(rlang::arg_match(into) == "cols", "wider", "longer")
     if (is_survey(data) && into == "longer") {
         rlang::abort("Cannot create new rows in surveys.")
     }
-    if (!inherits(by, c("integer", "numeric", "character")) || length(by) > 1) {
-        rlang::abort("Expecting `by` to be a string or a positive integer.")
-    } else if (is.numeric(by)) {
-        if (by != as.integer(by)) {
-            rlang::warn(sprintf("Rounding to `by = %s`.", as.integer(by)))
+    if (!inherits(by, c("integer", "numeric", "character"))) {
+        rlang::abort("Expecting `by` to be a string or positive integers.")
+    }
+    if (is.numeric(by)) {
+        if (any(by != as.integer(by))) {
+            rlang::warn("Rounding `by` down to nearest integer(s).")
         }
-        by <- as.integer(by)
-        var_nchar <- max(nchar(as.character(data[[var]])))
-        if (by < 1 || by >= var_nchar) {
+        by <- floor(by)
+        vnc <- max(nchar(as.character(data[[var]])))
+        if (any(by < 1) || any(by >= vnc)) {
             rlang::abort(paste(
                 "The value of `by` must be positive and strictly less than the",
                 "length of the longest string in the variable to be separated."
@@ -36,22 +41,24 @@ separate_var <- function(data, var, by, left, right,
     }
     method <- ifelse(is.character(by), "delim", "position")
     sep_fn <- sprintf("tidyr::separate_%s_%s", into, method)
-    sep_args <- list(rlang::sym(var))
+    sep_arg <- list(rlang::sym(var))
     if (grepl("wider_position", sep_fn)) {
-        by <- rlang::parse_expr(sprintf(
-            "c(%s = %s, %s = %s)",
-            left, by, right, var_nchar - by
-        ))
+        if (length(by) != length(names) - 1) {
+            rlang::abort("Length of `by` must be shorter than `names` by 1.")
+        }
+        names <- tidyr::replace_na(names, "")
+        by <- rlang::expr(c(!!!rlang::set_names(diff(c(0, by, vnc)), names)))
+    } else if (length(by) > 1) {
+        rlang::abort("Length of `by` must be 1.")
     }
-    sep_args <- c(sep_args, by)
+    sep_arg <- c(sep_arg, by)
     if (grepl("wider_delim", sep_fn)) {
-        sep_args <- sep_args |>
-            c(names = rlang::expr(c(!!left, !!right)), too_many = "merge")
+        sep_arg <- c(sep_arg, names = rlang::enexpr(names), too_many = "merge")
     }
     if (grepl("wider", sep_fn)) {
-        sep_args <- c(sep_args, too_few = "align_start")
+        sep_arg <- c(sep_arg, too_few = "align_start")
     }
-    sep_expr <- rlang::expr((!!rlang::parse_expr(sep_fn))(!!!sep_args))
+    sep_expr <- rlang::expr((!!rlang::parse_expr(sep_fn))(!!!sep_arg))
     if (is_survey(data)) {
         expr <- rlang::expr(!!expr %>% (\(x) {
             x$variables <- x$variables %>% !!sep_expr
--- tests/testthat/test_separate_var.R
+++ tests/testthat/test_separate_var.R
@@ -1,23 +1,32 @@
 data <- data.frame(
     "B" = c("hi", "hello", "bye"),
     "A" = c("ada.sd\\d-sd^fdsf", "B_37K", "C"),
-    "C" = c("2019M02", "2019M03", "2019M04")
+    "C" = c("2019M02", "2019M03", "2019M04"),
+    "D" = c("x.x", "x.x.x.x.x", "x.x.x")
 )
 
 
-test_that("Basic separate works", {
-    expect_silent(separate_var(data, "C", "M", "year", "month"))
-    expect_silent(separate_var(data, "C", 4, "year", "month"))
-    expect_silent(separate_var(data, "C", by = "M", into = "rows"))
-    expect_silent(separate_var(data, "C", by = 4, into = "rows"))
+test_that("Basic separation works", {
+    expect_silent(separate_var(data, "C", "M", c("year", "month")))
+    expect_silent(separate_var(data, "C", 4, c("year", "month")))
+    expect_silent(separate_var(data, "C", "M", into = "rows"))
+    expect_silent(separate_var(data, "C", 4, into = "rows"))
+})
+
+test_that("Multi-column separation works", {
+    expect_silent(separate_var(data, "D", ".", c("a", "b", "c")))
+    expect_silent(separate_var(data, "A", c(1, 3, 10), c("a", "b", "c", "d")))
 })
 
 test_that("Condition checks", {
-    expect_error(separate_var(data, "C", factor("M"), "year", "month"))
-    expect_warning(separate_var(data, "C", 4.1, "year", "month"))
-    expect_error(separate_var(data, "C", 11, "year", "month"))
-    expect_error(separate_var(data, "C", 0, "year", "month"))
-    expect_error(suppressWarnings(separate_var(data, "C", 0.9, "year", "month")))
+    expect_error(separate_var(data, "C", factor("M"), c("year", "month")))
+    expect_warning(separate_var(data, "C", 4.1, c("year", "month")))
+    expect_error(separate_var(data, "C", 11, c("year", "month")))
+    expect_error(separate_var(data, "C", 0, c("year", "month")))
+    expect_error(suppressWarnings(separate_var(data, "C", 0.9, c("year", "month"))))
+    expect_error(separate_var(data, "A", c(1, 3, 10), c("a", "b", "c")))
+    expect_error(separate_var(data, "C", 1:4, into = "rows"))
+    expect_error(separate_var(data, "C", c("M", "N"), c("year", "month")))
 })
 
 
@@ -28,7 +37,7 @@ svy <- svydesign(~ dnum + snum, weights = ~pw, fpc = ~ fpc1 + fpc2, data = apicl
 test_that("Survey designs work", {
     svy$variables$avg.ed <- format(svy$variables$avg.ed)
     suppressWarnings(
-        d <- separate_var(svy, "avg.ed", by = ".", "ed.a", "ed.b")
+        d <- separate_var(svy, "avg.ed", by = ".", c("ed.a", "ed.b"))
     )
     expect_s3_class(d, "survey.design2")
     expect_true(all(c("ed.a", "ed.b") %in% names(d$variables)))

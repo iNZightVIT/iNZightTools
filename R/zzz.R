ed93145620067c76e3579b6980f327e357af4741
Tom Elliott <tomelliottnz@gmail.com>
2023-03-06 09:07:27 +1300

    update tests, remove option set in onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,5 @@
 # nocov start
 .onLoad <- function(libname, pkgname) {
     # TODO: check what Suggested packages aren't installed and prompt to install them
-
-    if (is.null(getOption("inzighttools.comment"))) {
-        options(inzighttools.comment = "#")
-    }
 }
 # nocov end

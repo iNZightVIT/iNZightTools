0039e557c232648f73d7dc7c8d12cc73d3057603
Tom Elliott <tomelliottnz@gmail.com>
2024-05-03 10:17:52 +1200

    print more

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(iNZightTools)
 
-print(installed.packages()[, "Version"])
+print(installed.packages()[, 1:2])
 if (!requireNamespace("curl")) {
     stop("CURL IS NOT INSTALLED?!")
 }

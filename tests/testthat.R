5ff15a3a6b5336c4e27d69bccfc36c8f2ee664ab
Tom Elliott <tomelliottnz@gmail.com>
2024-05-03 10:32:58 +1200

    add token

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,4 @@
 library(testthat)
 library(iNZightTools)
 
-print(installed.packages()[, 1:2])
-if (!requireNamespace("curl")) {
-    stop("CURL IS NOT INSTALLED?!")
-}
-
 test_check("iNZightTools")
